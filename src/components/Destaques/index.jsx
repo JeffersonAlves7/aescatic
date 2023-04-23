fd386745a4d5b8669c7507941d223779378be6f6
JeffersonAlves7 <ilj031420@hotmail.com>
2023-04-23 00:08:02 -0300

    feat: update destaques

--- src/components/Destaques/index.jsx
+++ src/components/Destaques/index.jsx
@@ -1,3 +1,4 @@
+import { Carousel } from "react-bootstrap";
 
 function getDestaques() {
   return [
@@ -8,45 +9,20 @@ function getDestaques() {
 }
 
 export const Destaques = () => {
+  const destaques = getDestaques();
   return (
     <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {destaques.map((destaque) => {
+        return (
+          <Carousel.Item>
+            <div className="d-flex align-items-center justify-content-center">
+              <h2>
+                {destaque}
+              </h2>
+            </div>
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
 };
