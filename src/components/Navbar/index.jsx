b1528e04a6a75c22e420c410e564db26c03b155b
andressa-ricardo <andressaricardo30@gmail.com>
2023-04-22 23:33:28 -0300

    feat: add sass and ended navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,24 @@
 import logo from "../../assets/logo.png"
+import "./styles.scss"
 
-export function Navbar(){
+export function Navbar() {
     return (
         <div className="navbar">
-            <img src={logo} alt="" />
-            <input type="text" />
-            <i class="bi bi-suit-heart-fill"></i>
+            <a href="">
+                <img src={logo} alt="logo-image" className="logo" />
+            </a>
+            <div>
+                <input type="text" />
+                <button>
+                    <i class="bi bi-search"></i>
+                </button>
+            </div>
+
+            <div className="icons">
+                <i class="bi bi-person"></i>
+                <i class="bi bi-suit-heart"></i>
+                <i class="bi bi-cart"></i>
+            </div>
         </div>
     )
 }
