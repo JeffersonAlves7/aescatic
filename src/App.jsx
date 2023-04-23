c2510c233f24a3df3e74d7d9a65d263420c55050
andressa-ricardo <andressaricardo30@gmail.com>
2023-04-23 01:41:24 -0300

    feat: add roupas component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Destaques } from "./components/Destaques";
 import { Navbar } from "./components/Navbar";
-
+import { Roupas } from "./components/Roupas";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Destaques />
+      <Roupas />
     </div>
   );
 }
---
+++ src/components/Roupas/index.jsx
@@ -0,0 +1,24 @@
+import { flex } from "./styles.module.css";
+
+
+export function Roupas() {
+    return (
+        <div>
+            <div className={flex}>
+                <h1>Croppeds</h1>
+                <div>
+                    <label htmlFor="">Filtrar por:</label>
+                    <select className="filter">
+                        <option value="">Recomendar</option>
+                        <option value="maior">Maior preço</option>
+                        <option value="">Menor preço</option>
+                    </select>
+                </div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
