451e31b8900d3ee0b7b6b676dd213b283d553aa2
andressa-ricardo <andressaricardo30@gmail.com>
2023-04-23 00:06:22 -0300

    feat: update navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,20 +1,20 @@
 import logo from "../../assets/logo.png"
-import "./styles.scss"
+import {navbar, icons, logoImage, buscar} from "./styles.module.css"
 
 export function Navbar() {
     return (
-        <div className="navbar">
+        <div className={navbar}>
             <a href="">
-                <img src={logo} alt="logo-image" className="logo" />
+                <img src={logo} alt="logo-image" className={logoImage} />
             </a>
-            <div>
-                <input type="text" />
+            <div className={buscar}>
+                <input type="text" placeholder="Buscar"/>
                 <button>
                     <i class="bi bi-search"></i>
                 </button>
             </div>
 
-            <div className="icons">
+            <div className={icons}>
                 <i class="bi bi-person"></i>
                 <i class="bi bi-suit-heart"></i>
                 <i class="bi bi-cart"></i>
