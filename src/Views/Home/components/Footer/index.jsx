137fd9b030d0c6249c1813c610bf30ca554a3e23
JeffersonAlves7 <ilj031420@hotmail.com>
2023-04-24 02:23:23 -0300

    feat: update footer component with new css and classNames

--- src/Views/Home/components/Footer/index.jsx
+++ src/Views/Home/components/Footer/index.jsx
@@ -1,37 +1,47 @@
-import { wpp, flex, email, foter, instagram } from "./styles.module.css";
+import { wpp, email, footer, instagram } from "./styles.module.css";
 
 export function Footer() {
   return (
-    <footer className={foter}>
-      <div className={flex}>
+    <footer className={footer}>
+      <div className="Contatos">
         <h4>Contato</h4>
-        <div className="whatsapp">
-          <label htmlFor="">Whatsapp</label>
-          <a className={flex} href="">
-            <div className={wpp}>
-              <i class="bi bi-whatsapp"></i>
-            </div>
-            <a href="https://wa.me/5521982993019">(21)96590-3512</a>
-          </a>
-        </div>
-        <div className="email">
-          <label htmlFor="">Email</label>
-          <a className={flex} href="">
-            <div className={email}>
-              <i class="bi bi-envelope-at-fill"></i>
-            </div>
-            <a href="mailto:andressaricardo30@gmail.com">
+        <div className="d-flex align-items-center gap-5">
+          <div className="whatsapp">
+            <span>Whatsapp</span>
+            <a
+              className="d-flex align-items-center gap-2"
+              href="https://wa.me/5521982993019"
+            >
+              <div className={wpp}>
+                <i class="bi bi-whatsapp"></i>
+              </div>
+              (21)96590-3512
+            </a>
+          </div>
+          <div className="email">
+            <span>Email</span>
+            <a
+              className="d-flex align-items-center gap-2"
+              href="mailto:andressaricardo30@gmail.com"
+            >
+              <div className={email}>
+                <i class="bi bi-envelope-at-fill"></i>
+              </div>
               andressaricardo30@gmail.com
             </a>
-          </a>
+          </div>
         </div>
       </div>
-      <div>
+      <div className="redes">
         <h4>Redes sociais</h4>
-        <div className={instagram}>
-          <i class="bi bi-instagram"></i>
+        <div className="instagram">
+          <a className="d-flex align-items-center gap-2" href="">
+            <div className={instagram}>
+              <i class="bi bi-instagram"></i>
+            </div>
+            <span>Instagram</span>
+          </a>
         </div>
-        <a href="">Instagram</a>
       </div>
     </footer>
   );
