74e8f1bcf2fb74653151a47237374d2897ced48a
JeffersonAlves7 <ilj031420@hotmail.com>
2023-04-25 02:03:06 -0300

    feat: update navbar add position fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+
 import { Home } from './Views/Home';
 
 import { Catalogo } from './Views/Catalogo';
@@ -9,9 +11,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-
           <Route path="/catalogo" element={<Catalogo />} />
           <Route path="/catalogo/:categoria" element={<Categoria />} />
           <Route path="/catalogo/:categoria/:peca" element={<Info />} />
--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,12 +1,10 @@
 import { Destaques } from './components/Destaques';
-import { Navbar } from './components/Navbar';
 import { Sobre } from './components/Sobre';
 import { Footer } from './components/Footer';
 
 export function Home() {
   return (
     <div id="Home">
-      <Navbar />
       <Destaques />
       <Sobre />
       <Footer />
