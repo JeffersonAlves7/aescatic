5bc56dfa516f8bc72bfc757135cc6cd2bb7906f4
JeffersonAlves7 <ilj031420@hotmail.com>
2023-04-25 02:37:50 -0300

    feat: update view categoria

--- src/Views/Catalogo/Categoria/index.jsx
+++ src/Views/Catalogo/Categoria/index.jsx
@@ -1,13 +1,67 @@
 import { useParams } from 'react-router-dom';
+import {
+  Produto,
+  Produtos,
+  ProdutoName,
+  ProdutoPrice,
+  ProdutoImage,
+} from './styles.module.css';
+
+function getByCategory(categoria) {
+  console.log(categoria);
+  const produtos = [
+    {
+      name: 'CROPPED VERMELHO',
+      price: 30.0,
+      img: '',
+    },
+    {
+      name: 'CROPPED VERMELHO',
+      price: 30.0,
+      img: '',
+    },
+    {
+      name: 'CROPPED VERMELHO',
+      price: 30.0,
+      img: '',
+    },
+    {
+      name: 'CROPPED VERMELHO',
+      price: 30.0,
+      img: '',
+    },
+    {
+      name: 'CROPPED VERMELHO',
+      price: 30.0,
+      img: '',
+    },
+  ];
+
+  return produtos.map((produto) => {
+    const price = produto.price;
+    return { ...produto, price: `R$ ${price}` };
+  });
+}
 
 export function Categoria() {
   const { categoria } = useParams();
+  const produtos = getByCategory(categoria);
 
   return (
     <div id="categoria">
-      <h1>Categoria</h1>
-      <div>
-        <p>{categoria}</p>
+      <div className="container">
+        <h1>Categoria</h1>
+        <div className={Produtos}>
+          {produtos.map((p, i) => (
+            <div key={`${p}-${i}`} className={Produto}>
+              <div className={ProdutoImage}>
+                <img src={p.img} alt="" />
+              </div>
+              <p className={ProdutoName}>{p.name}</p>
+              <p className={ProdutoPrice}>{p.price}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
