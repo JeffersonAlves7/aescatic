695dd6b408a767b1672a22793e3de521cd086f36
andressa-ricardo <andressaricardo30@gmail.com>
2023-04-24 23:41:58 -0300

    feat: update info view

--- src/Views/Catalogo/Info/index.jsx
+++ src/Views/Catalogo/Info/index.jsx
@@ -1,4 +1,12 @@
 import { useParams } from 'react-router-dom';
+import {
+  viewImg,
+  imgsR,
+  centralImg,
+  flex,
+  btnTamanho,
+  infos,
+} from './styles.module.css';
 
 function getInfo() {
   return {};
@@ -10,11 +18,42 @@ export function Info() {
   const info = getInfo(); // Essa variavel vai ter todas as informacoes sobre o produto
 
   return (
-    <div id="info">
-      <h1>info</h1>
-      <div>
-        <p>{categoria}</p>
-        <p>{peca}</p>
+    <div>
+      <h1>Cropped Vermelho</h1>
+      <div className={infos}>
+        <div className={flex}>
+          <div className={imgsR}>
+            <div className={viewImg}></div>
+            <div className={viewImg}></div>
+            <div className={viewImg}></div>
+          </div>
+          <img className={centralImg} src="" alt="" />
+        </div>
+        <div>
+          <div>
+            <p>R$ 30,00</p>
+            <span>3x de 10 sem juros</span>
+            <p>Tamanho:</p>
+            <div className={btnTamanho}>
+              <button>PP</button>
+              <button>P</button>
+              <button>M</button>
+              <button>G</button>
+              <button>GG</button>
+            </div>
+            <p>Cor:</p>
+            <select name="" id="">
+              <option value="">azul</option>
+            </select>
+            <p>Quantidade:</p>
+            <div className={flex}>
+              <input type="number" />
+              <i className="bi bi-suit-heart"></i>
+              <i className="bi bi-cart"></i>
+            </div>
+            <button>Comprar Agora</button>
+          </div>
+        </div>
       </div>
     </div>
   );
